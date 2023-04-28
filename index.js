f3cb0a96e9b072ab8a43be833cc1e95b509bf527
Chad Rajcooar <chadr208@gmail.com>
2023-04-28 16:50:12 -0400

    Finished init functions, modified choices for shape use, created examples folder to direct svg to be placed

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const questions = [
     {
         type: 'list',
         message: 'What shape would you like to use?',
+        choices: ['Square', 'Triangle', 'Circle'],
         name: 'shape',
     },
     {
@@ -26,13 +27,48 @@ const questions = [
         name: 'color',
     },
 ];
+// generates svg and places in examples
+function writeToFile(shape) {
+    const svgText = `
+    <svg version="1.1"
+     width="300" height="200"
+     xmlns="http://www.w3.org/2000/svg">
 
-function writeToFile() {
-    fs.writeFile();
+    ${shape.render()}
+
+    </svg>
+    `;
+    fs.writeFile("./examples/logo.svg", svgText, (err) => err ? console.log("Error: Failed to generate logo.svg") : console.log("Generated logo.svg"));
 }
 
 function init() {
-    inquirer.createPromptModule(questions).then((response) => {
-        writeToFile();
+    inquirer.prompt(questions).then((response) => {
+        const { text, textColor, shape, color } = response;
+        // console log error for more than 3 characters
+        if (text.length > 3) {
+            console.log("Error: Only 3 characters can be entered - Enter up to 3 characters");
+            return new Error("Error: Only 3 characters can be entered - Enter up to 3 characters");
+        }
+        else {
+            // Chooses user selected shape
+            let newShape;
+            switch (shape) {
+                case "Circle":
+                    newShape = new Circle(text, textColor, color);
+                    break;
+                case "Square":
+                    newShape = new Square(text, textColor, color);
+                    break;
+                case "Triangle":
+                    newShape = new Triangle(text, textColor, color);
+                    break;
+                default:
+                    newShape = new Circle(text, textColor, color);
+                    break;
+            }
+            writeToFile(newShape);
+        }
     });
 }
+// Initialization
+init();
